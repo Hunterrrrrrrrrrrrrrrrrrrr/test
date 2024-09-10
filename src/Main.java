f039092bfc416c0c0c82d2115c1338df2d2d12ee
Lenovo <12212910@mail.sustech.edu.cn>
2024-09-10 17:19:15 +0800

    更改

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,82 @@
+import java.util.*;
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        EmployeeManagement management = new EmployeeManagement();
+
+        management.addEmployee("A", 25);
+        management.addEmployee("B", 30);
+        management.addEmployee("C", 22);
+
+        management.deleteEmployee("B");
+
+        management.addEmployee("A", 25);
+        management.addEmployee("D", 27);
+
+        management.printEmployees();
     }
 }
+
+
+class Employee {
+    private String name;
+    private int age;
+
+    public Employee(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{name='" + name + "', age=" + age + '}';
+    }
+}
+
+class EmployeeManagement {
+    private List<Employee> employees;
+
+    public EmployeeManagement() {
+        this.employees = new ArrayList<>();
+    }
+
+    public void addEmployee(String name, int age) {
+        for (Employee employee : employees) {
+            if (employee.getName().equals(name)) {
+                return;
+            }
+        }
+        employees.add(new Employee(name, age));
+    }
+
+    public void deleteEmployee(String name) {
+        Iterator<Employee> iterator = employees.iterator();
+        while (iterator.hasNext()) {
+            Employee employee = iterator.next();
+            if (employee.getName().equals(name)) {
+                iterator.remove();
+                break;
+            }
+        }
+    }
+
+    public void printEmployees() {
+        // Sort by name
+        Collections.sort(employees, Comparator.comparing(Employee::getName));
+        // Print employees
+        for (Employee employee : employees) {
+            System.out.println(employee);
+        }
+    }
+}
+
+
